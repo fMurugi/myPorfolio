2dff32a32ccb43244f81a7b13b86de761155b339
fMurugi <fionamurugi99@gmail.com>
2022-09-07 17:04:58 +0300

    Added sanity configiration and Added skills from the sanity

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+
+import { urlFor,client } from '../../client';
 
 import './Skills.scss'
 
 function Skills() {
  const languages =['html','css','Javascript','React js','Node js','Java','Spring','Mysql','postgresql']
 
+  const [skills,setSkills]=useState([]);
+  useEffect(()=>{
+    const query = '*[_type=="skills"]';
+
+    client.fetch(query)
+      .then((data) =>{setSkills(data);
+        console.log(data);
+      })
+      
+  },[]);
+ 
   return (
     <div className='skills__container'>
       <h1>SKILLS</h1>
       <div className='skills__list'>
         <ul>
-          {languages.map((language)=> <li><div className='list__container'>{language}</div></li>)}
+          {skills.map((skill)=> <li><div className='list__container'>
+            <img src={urlFor(skill.icon)}/>
+            {skill.name}
+            </div></li>)}
         </ul>
       </div>
     
